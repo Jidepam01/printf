7dd6ca5f59c40ebf41f542789857d2c80fe22c2f
samuel <delighteousruler@yahoo.com>
2023-06-27 10:23:02 -0700

    fixed

--- _printf.c
+++ _printf.c
@@ -8,107 +8,39 @@ int _printf(const char *format, ...)
 {
     va_list args;
     va_start(args, format);
-
     int count = 0;
-    char ch;
 
-    for (int i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-
-            switch (format[i])
-            {
-                case 'd':
-                {
-                    int value = va_arg(args, int);
-                    int num_digits = 0;
-                    int temp = value;
-
-                    if (temp < 0)
-                    {
-                        _putchar('-');
-                        count++;
-                        temp = -temp;
-                    }
-
-                    while (temp != 0)
-                    {
-                        num_digits++;
-                        temp /= 10;
-                    }
-
-                    if (value == 0)
-                    {
-                        _putchar('0');
-                        count++;
-                    }
-
-                    while (value != 0)
-                    {
-                        int digit = value % 10;
-                        _putchar('0' + digit);
-                        count++;
-                        value /= 10;
-                    }
-
-                    count += num_digits;
-                    break;
-                }
-                case 'f':
-                {
-                    double value = va_arg(args, double);
-                    int int_part = (int)value;
-                    int decimal_part = (int)((value - int_part) * 1000000);
-
-                    if (value < 0)
-                    {
-                        _putchar('-');
-                        count++;
-                        int_part = -int_part;
-                        decimal_part = -decimal_part;
-                    }
-
-                    count += printf("%d.", int_part);
-                    count += printf("%06d", decimal_part);
-                    break;
-                }
-                case 'c':
-                {
-                    ch = (char)va_arg(args, int);
-                    _putchar(ch);
+	while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+              switch (*format) {
+                case 'c': {
+                    char c = (char) va_arg(args, int);
+                    _putchar(c);
                     count++;
                     break;
                 }
-                case 's':
-                {
-                    char *str = va_arg(args, char*);
-                    int j = 0;
-                    while (str[j] != '\0')
-                    {
-                        _putchar(str[j]);
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    while (*s != '\0') {
+                        _putchar(*s);
+                        s++;
                         count++;
-                        j++;
                     }
                     break;
                 }
-                default:
-                {
+                case '%':
                     _putchar('%');
-                    _putchar(format[i]);
-                    count += 2;
+                    count++;
                     break;
-                }
             }
-        }
-        else
-        {
-            _putchar(format[i]);
+        } else {
+            _putchar(*format);
             count++;
         }
+        format++;
     }
-
     va_end(args);
-    return(count);
+    return count;
 }
+
