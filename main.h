7b787c0578ded7f50b6b1e3cb4f7f0f426db3577
samuel <delighteousruler@yahoo.com>
2023-06-27 03:59:53 -0700

    funct

--- main.h
+++ main.h
@@ -1,16 +1,40 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
 
+int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size);
 
+UNUSED(types);
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
 
+int print_int(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 
+UNUSED(buffer);
+UNUSED(flags);
+UNUSED(width);
+UNUSED(precision);
+UNUSED(size);
 
 
 #endif
